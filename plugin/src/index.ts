a69f62565ad4d719b2261c8d05d05ac0687b57cd
Adinnu Benedict <benidixt@gmail.com>
2024-12-04 14:09:26 +0100

    Updater Plugin

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -17,15 +17,34 @@ const withBluetoothPermissions: ConfigPlugin = (config) => {
   config = withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
 
-    androidManifest.manifest["uses-permission"] = [
-      ...(androidManifest.manifest["uses-permission"] || []),
-      { $: { "android:name": "android.permission.BLUETOOTH" } },
-      { $: { "android:name": "android.permission.BLUETOOTH_ADMIN" } },
-      { $: { "android:name": "android.permission.BLUETOOTH_SCAN" } },
-      { $: { "android:name": "android.permission.BLUETOOTH_CONNECT" } },
-      { $: { "android:name": "android.permission.ACCESS_FINE_LOCATION" } },
-      { $: { "android:name": "android.permission.USB_PERMISSION" } },
+    if (!androidManifest || !androidManifest.manifest) {
+      throw new Error(
+        "AndroidManifest.xml is not properly parsed. Please check your configuration."
+      );
+    }
+
+    const permissionsToAdd = [
+      "android.permission.BLUETOOTH",
+      "android.permission.BLUETOOTH_ADMIN",
+      "android.permission.BLUETOOTH_SCAN",
+      "android.permission.BLUETOOTH_CONNECT",
+      "android.permission.ACCESS_FINE_LOCATION",
+      "android.permission.USB_PERMISSION",
     ];
+
+    const existingPermissions =
+      androidManifest.manifest["uses-permission"]?.map(
+        (permission) => permission.$["android:name"]
+      ) || [];
+
+    permissionsToAdd.forEach((permission) => {
+      if (!existingPermissions.includes(permission)) {
+        androidManifest.manifest["uses-permission"] = [
+          ...(androidManifest.manifest["uses-permission"] || []),
+          { $: { "android:name": permission } },
+        ];
+      }
+    });
 
     return config;
   });
